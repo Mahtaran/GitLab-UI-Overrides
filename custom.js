d640a3eaaaf90c879bf85d2b403421333b588021
Mahtaran <mahtaran@amuzil.com>
2019-08-20 01:39:02 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,67 +1,72 @@
+function executeWithCookies(callback) {
+	if (typeof Cookies === "undefined") $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js", callback);
+	else callback();
+}
+
+const COOKIE_PREFIX = "mahtaran_gitlab_ui_overrides_";
+const COOKIE_CUSTOM_SELECTED_THEME = COOKIE_PREFIX + "custom_selected_theme";
+
+const DEFAULT_THEMES = ["ui-indigo", "ui-light-indigo", "ui-blue",
+			"ui-light-blue", "ui-green", "ui-light-green",
+			"ui-red", "ui-light-red", "ui-dark", "ui-light"]
+const THEMES = DEFAULT_THEMES.slice();
+
 $(document).ready(function() {
 	var oldContent = $("title").html();
 	$("title").html(oldContent.replace(new RegExp("GitLab", "g"), "Amuzil Git"));
 	
-	//$("body").removeClass();
-	//$("body").addClass();
+	// Themes
+	addTheme("Amuzil");
 	
-	executeWithCookies(function() {
-		var theme = Cookies.get("mahtaran_gitlab_ui_overrides_custom_selected_theme");
-		if (theme !== undefined) {
-			var classes = $("body").attr("class").split(/\s+/);
-			var elements = $(".col-lg-8.application-theme").find("label");
-			var availableThemes = new Array(elements.length);
-			var newTheme = "ui-amuzil";
-			
-			elements.each(function() {
-				var elementTheme = $(this).find("div").attr("class").split(/\s+/)[1];
-				availableThemes.push(elementTheme);
-				if (elementTheme === theme) {
-					$(this).find("input").prop("checked", true);
-					newTheme = elementTheme;
-				} else {
-					$(this).find("input").prop("checked", false);	
-				}
-			});
-			
-			var newClasses = classes.filter(clazz => !availableThemes.includes(clazz));
-			newClasses.push(newTheme);
-			
-			console.log(classes);
-			console.log(newClasses);
-			
-			$("body").attr("class", newClasses.join(" "));
-		}
+	$(".col-lg-8.application-theme").on("click", "label", function(event) {
+		setTheme($(this).find("div").attr("class").split(/\s+/)[1]);
+		event.preventDefault();
 	});
+	
+	// Set correct theme
+	setTheme();
+});
+
+function addTheme(theme) {
+	const theme_identifier = "ui-" + theme.toLowerCase().replace(/\s+/, "-");
+	const theme_number = THEMES.length + 1; // Not necessary, but here for completeness.
 	
 	$(".col-lg-8.application-theme").append(`
 <label>
-	<div class="preview ui-amuzil"></div>
-	<input type="radio" value="11" name="user[theme_id]" id="user_theme_id_11">
-	Amuzil
+	<div class="preview ${theme_identifier}"></div>
+	<input type="radio" value="${theme_number}" name="user[theme_id]" id="user_theme_id_${theme_number}">
+	${theme}
 </label>
 	`);
 	
-	$(".col-lg-8.application-theme").on("click", "label", function(event) {
-		var theme = $(this).find("div").attr("class").split(/\s+/)[1];
-		onSelectTheme(theme);
-		event.preventDefault();
-	});
-});
-
-function onSelectTheme(theme) {
-	console.log(theme);
-	$(".col-lg-8.application-theme").find("label").each(function() {
-		if ($(this).find("div").attr("class").split(/\s+/)[1] === theme) {
-			$(this).find("input").prop("checked", true);
-			Cookies.set("mahtaran_gitlab_ui_overrides_custom_selected_theme", theme);
-		} else {
-			$(this).find("input").prop("checked", false);	
-		}
-	});
+	THEMES.push(theme_identfier);
 }
 
-function executeWithCookies(callback) {
-	if (typeof Cookies === "undefined") $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js", callback);
-	else callback();
+function setTheme(theme) {
+	executeWithCookies(function() {
+		if (typeof theme === "undefined") theme = Cookies.get(COOKIE_CUSTOM_SELECTED_THEME);
+		else Cookies.set(COOKIE_CUSTOM_SELECTED_THEME, theme);
+		
+		if (typeof theme === "undefined") return;
+		
+		const classes = $("body").attr("class").split(/\s+/);
+		
+		// Only used on the preferences page
+		$(".col-lg-8.application-theme").find("label").each(function() {
+			if ($(this).find("div").attr("class").split(/\s+/)[1] === theme) {
+				$(this).find("input").prop("checked", true);
+			} else {
+				$(this).find("input").prop("checked", false);	
+			}
+		});
+		
+		const newClasses = classes.filter(clazz => !THEMES.includes(clazz));
+		newClasses.push(theme);
+		
+		// Debugging
+		console.log(classes);
+		console.log(newClasses);
+		
+		$("body").attr("class", newClasses.join(" "));
+	});
 }
