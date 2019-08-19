0e2f3508bf7f15142d69fff4c5a749b0d3f8a198
Mahtaran <mahtaran@amuzil.com>
2019-08-19 21:42:00 +0200

    Whoops

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     var oldContent = $("title").html();
-    $("title").html(oldContent.replace(new RegExp("GitLab", "g"), "Amuzil Git");
+    $("title").html(oldContent.replace(new RegExp("GitLab", "g"), "Amuzil Git"));
 });
