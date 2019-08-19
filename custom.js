6205edf9075bc551498c71a94bb7a6552b929da4
Mahtaran <mahtaran@amuzil.com>
2019-08-19 22:57:24 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
-    var oldContent = $("title").html();
-    $("title").html(oldContent.replace(new RegExp("GitLab", "g"), "Amuzil Git"));
-    $("body").removeClass();
-    $("body").addClass();
-    
-    $(".col-lg-8.application-theme").append(`
+	var oldContent = $("title").html();
+	$("title").html(oldContent.replace(new RegExp("GitLab", "g"), "Amuzil Git"));
+	$("body").removeClass();
+	$("body").addClass();
+	
+	$(".col-lg-8.application-theme").append(`
 <label>
-    <div class="preview ui-amuzil"></div>
-    <input type="radio" value="11" name="user[theme_id]" id="user_theme_id_11">
-    Amuzil
+	<div class="preview ui-amuzil"></div>
+	<input type="radio" value="11" name="user[theme_id]" id="user_theme_id_11">
+	Amuzil
 </label>
-    `);
-    
-    $(".col-lg-8.application-theme").on("click", "label", function(event) {
-		onSelectTheme($(this).find("div").attr("class").split(/\s+/)[1]);
+	`);
+	
+	$(".col-lg-8.application-theme").on("click", "label", function(event) {
+		var theme = $(this).find("div").attr("class").split(/\s+/)[1]);
+		onSelectTheme(theme);
 		event.preventDefault();
-    });
+	});
 });
 
 function onSelectTheme(theme) {
@@ -23,8 +24,8 @@ function onSelectTheme(theme) {
 }
 
 function onSelectAmuzilTheme() {
-    if (typeof Cookies === "undefined") $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js", onSelectAmuzilTheme);
-    else {
-        Cookies.set("mahtaran-custom_selected_theme", "amuzil-theme"
-    }
+	if (typeof Cookies === "undefined") $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js", onSelectAmuzilTheme);
+	else {
+		Cookies.set("mahtaran-custom_selected_theme", "amuzil-theme"
+	}
 }
