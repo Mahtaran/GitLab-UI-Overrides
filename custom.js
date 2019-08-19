9b4f854b941e1902aecf03a19eee9b86cb0a0b1e
Mahtaran <mahtaran@amuzil.com>
2019-08-19 22:35:55 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
     var oldContent = $("title").html();
     $("title").html(oldContent.replace(new RegExp("GitLab", "g"), "Amuzil Git"));
+    $("body").removeClass();
+    $("body").addClass();
+    
+    $("col-lg-8 application-theme").append(`
+<label>
+    <div class="preview ui-amuzil"></div>
+    <input type="radio" onclick="onSelectAmuzilTheme">
+    Amuzil
+</label>
+    `);
 });
+
+function onSelectAmuzilTheme() {
+    if (typeof Cookies === "undefined") $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js");
+    print(Cookies.get());
+}
